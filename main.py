def7f6b7df84bef816ac8741015c31a22f2447bb
burakhanaksoy <burakhan.aksoy@tedu.edu.tr>
2021-04-11 10:03:59 +0300

    compression done, from 110MB -> 5.6MB

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import zipfile
 
+# Context manager
+with zipfile.ZipFile('after_compression.zip', 'w',compression=zipfile.ZIP_DEFLATED,compresslevel=9) as demo_zip:
+    demo_zip.write('large_files/100MB.txt')
+    demo_zip.write(
+        'large_files/Very_Large_Telescope_Ready_for_Action_(ESO).jpeg')
+    
